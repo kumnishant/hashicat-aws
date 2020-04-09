49bab3dec8153fbd7d7a70929d36f4d3aeb69508
kumar nishant <kumnishant@gmail.com>
2020-04-09 21:37:58 +0000

    Updated my code.

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
 module "vpc" {
   source  = "app.terraform.io/kumar-training/vpc/aws"
   version = "2.33.0"
-  name = "my-vpc"
+  name = "my-vpc1"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/16", "10.0.2.0/16", "10.0.3.0/16"]
-  public_subnets  = ["10.0.101.0/16", "10.0.102.0/16", "10.0.103.0/16"]
+  private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  public_subnets  = ["10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
