e36be0d28acd2ef564082d6d7a2b6d993fc15602
kumar nishant <kumnishant@gmail.com>
2020-04-09 22:15:03 +0000

    Updated my code.

--- main.tf
+++ main.tf
@@ -77,7 +77,7 @@ resource aws_route_table "hashicat" {
   vpc_id = aws_vpc.hashicat.id
 
   route {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.hashicat.id
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,11 @@ module "vpc" {
   source  = "app.terraform.io/kumar-training/vpc/aws"
   version = "2.33.0"
   name = "my-vpc1"
-  cidr = "10.0.0.0/16"
+  cidr = "0.0.0.0/0"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+  private_subnets = ["0.0.0.0/24", "0.0.1.0/24", "0.0.2.0/24"]
+  public_subnets  = ["0.0.3.0/24", "0.0.4.0/24", "0.0.5.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
