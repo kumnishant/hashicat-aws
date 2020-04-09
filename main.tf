11e9d7515844bababb736d72330bc7b5b44b7365
kumar nishant <kumnishant@gmail.com>
2020-04-09 22:11:46 +0000

    Updated my code.

--- main.tf
+++ main.tf
@@ -77,7 +77,7 @@ resource aws_route_table "hashicat" {
   vpc_id = aws_vpc.hashicat.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "10.0.0.0/16"
     gateway_id = aws_internet_gateway.hashicat.id
   }
 }
