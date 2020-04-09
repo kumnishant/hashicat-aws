d4077d99ec9bf9f9613dfff7949b63048f81a758
kumar nishant <kumnishant@gmail.com>
2020-04-09 22:29:28 +0000

    Updated my code.

--- vpc.tf
+++ vpc.tf
@@ -4,9 +4,9 @@ module "vpc" {
   name = "my-vpc1"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.2.4.0/24", "10.2.5.0/24", "10.2.6.0/24"]
-  public_subnets  = ["10.2.104.0/24", "10.2.105.0/24", "10.2.106.0/24"]
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.2.4.0/24", "10.2.5.0/24"]
+  public_subnets  = ["10.2.104.0/24", "10.2.105.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
